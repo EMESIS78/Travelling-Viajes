0c5d2f422d0d8336278b9b93b3273b03efa56b8b
EMESIS78 <fabrismal351@gmail.com>
2025-03-18 22:18:41 -0500

    implementacion del drawer navigation

--- Navigations/navigationTypes.tsx
+++ Navigations/navigationTypes.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from '@react-navigation/native';
 
 import HomeScreen from '../app/views/HomeScreen';
@@ -19,7 +20,7 @@ export type StackParamList = {
 // Crear Stack Navigator
 const Stack = createNativeStackNavigator();
 
-const AppNavigator = () => {
+const StackNavigator  = () => {
     return (
 
         <Stack.Navigator initialRouteName="Home">
@@ -32,5 +33,26 @@ const AppNavigator = () => {
 
     );
 };
+
+const Drawer = createDrawerNavigator();
+
+const DrawerNavigator = () => {
+  return (
+    <Drawer.Navigator initialRouteName="Home">
+      <Drawer.Screen name="Home" component={StackNavigator} options={{ title: "Inicio" }} />
+      <Drawer.Screen name="Alojamientos" component={AlojamientosScreen} />
+      <Drawer.Screen name="Tours" component={ToursScreen} options={{ title: "Explorar Tours" }} />
+      <Drawer.Screen name="Transporte" component={TransporteScreen} />
+      <Drawer.Screen name="Restaurantes" component={RestaurantesScreen} />
+    </Drawer.Navigator>
+  );
+};
+
+const AppNavigator = () => {
+    return (
+        <DrawerNavigator />
+    );
+  };
+  
 
 export default AppNavigator;
